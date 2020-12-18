640f51475f77faec044ae80820ed4831c9182bad
Vlad Bezden <vlad.bezden@gmail.com>
2020-12-18 10:46:17 -0500

    Code refactoring.

--- problems/7. Reverse Integer/reverse_integer.py
+++ problems/7. Reverse Integer/reverse_integer.py
@@ -5,7 +5,7 @@ from timeit import timeit
 
 class Solution:
     def reverse(self, x: int) -> int:
-        x = int(str(x)[::-1]) if x >= 0 else -int(str(abs(x))[::-1])
+        x = int(str(x)[::-1]) if x >= 0 else -int(str(-x)[::-1])
         return 0 if x >= 2 ** 31 - 1 or x <= -(2 ** 31) else x
 
     def reverse_2(self, x: int) -> int:
