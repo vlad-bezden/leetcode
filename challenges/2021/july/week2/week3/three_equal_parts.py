91c914340609a17a28eb653fd815323f69cc2bdc
Vlad Bezden <vlad.bezden@gmail.com>
2021-07-17 13:11:54 -0400

    Add solution for '927. Three Equal Parts' problem

---
+++ challenges/2021/july/week2/week3/three_equal_parts.py
@@ -0,0 +1,53 @@
+"""Three Equal Parts.
+
+    https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3817/
+"""
+from operator import indexOf
+
+
+class Solution:
+    @staticmethod
+    def threeEqualParts(arr: list[int]) -> list[int]:
+        no_ans = [-1, -1]
+        numerator, denominator = divmod(sum(arr), 3)
+        if not numerator and not denominator:
+            # list of all 0's
+            return [0, len(arr) - 1]
+        if denominator:
+            # it can't be split, number of 1's must be divisible by 3
+            return no_ans
+        # find trailing 0's
+        zeros = indexOf(reversed(arr), 1)
+        # find all 1's indexes
+        ones_indexes = tuple(i for i, v in enumerate(arr) if v)
+        # find start/end for each length of 1s
+        p1s, p1e = ones_indexes[0], ones_indexes[numerator - 1]
+        p2s, p2e = ones_indexes[numerator], ones_indexes[2 * numerator - 1]
+        p3s, p3e = ones_indexes[2 * numerator], ones_indexes[-1]
+        # get each part including leading 0's
+        part1 = arr[p1s : p1e + 1 + zeros]
+        part2 = arr[p2s : p2e + 1 + zeros]
+        part3 = arr[p3s : p3e + 1 + zeros]
+        # all parts has to be the same
+        if part1 != part2 or part2 != part3:
+            return no_ans
+        return [p1e + zeros, p2e + zeros + 1]
+
+
+if __name__ == "__main__":
+    inputs = (
+        ([1, 0, 1, 0, 1], [0, 3]),
+        ([1, 1, 0, 1, 1], [-1, -1]),
+        ([1, 1, 0, 0, 1], [0, 2]),
+        ([1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1], [2, 8]),
+        ([1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], [3, 8]),
+        ([1, 0, 1, 0, 0, 1, 0, 0], [-1, -1]),
+        ([0, 0, 0, 0, 0], [0, 4]),
+        ([0, 1, 0, 1, 1, 0, 1, 1, 0, 1], [3, 7]),
+        ([1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1], [-1, -1]),
+    )
+    for input, expected in inputs:
+        output = Solution.threeEqualParts(input)
+        assert output == expected, f"{input=}, {output=}, {expected=}"
+
+    print("PASSED!!!")
---
+++ problems/927. Three Equal Parts/README.md
@@ -0,0 +1,43 @@
+## [927. Three Equal Parts](https://leetcode.com/problems/three-equal-parts/)
+### Hard
+
+You are given an array `arr` which consists of only zeros and ones, divide the
+array into `three non-empty parts` such that all of these parts
+represent the same binary value.
+
+If it is possible, return any `[i, j]` with `i + 1 < j`, such that:
+
+* `arr[0], arr[1], ..., arr[i]` is the first part,
+* `arr[i + 1], arr[i + 2], ..., arr[j - 1]` is the second part, and
+* `arr[j], arr[j + 1], ..., arr[arr.length - 1]` is the third part.
+* All three parts have equal binary values.
+
+If it is not possible, return `[-1, -1]`.
+
+Note that the entire part is used when considering what binary value it represents.
+For example, `[1,1,0]` represents `6` in decimal, not `3`.
+Also, leading zeros are allowed, so `[0,1,1]` and `[1,1]` represent the same value.
+
+
+
+__Example 1:__
+```
+Input: arr = [1,0,1,0,1]
+Output: [0,3]
+```
+
+__Example 2:__
+```
+Input: arr = [1,1,0,1,1]
+Output: [-1,-1]
+```
+
+__Example 3:__
+```
+Input: arr = [1,1,0,0,1]
+Output: [0,2]
+```
+
+__Constraints:__
+* 3 <= arr.length <= 3 * 104
+* arr[i] is 0 or 1
