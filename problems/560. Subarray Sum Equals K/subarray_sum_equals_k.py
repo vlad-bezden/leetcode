12b1f9cdd46bfc568a0a3ed59b2a16fdab737c91
Vlad Bezden <vlad.bezden@gmail.com>
2021-07-17 20:29:53 -0400

    Add solution for '560. Subarray Sum Equals K' problem

--- problems/560. Subarray Sum Equals K/subarray_sum_equals_k.py
+++ problems/560. Subarray Sum Equals K/subarray_sum_equals_k.py
@@ -2,48 +2,28 @@
 
     https://leetcode.com/problems/subarray-sum-equals-k/
 """
-from collections import deque
 
 
 class Solution:
     @staticmethod
     def subarraySum(nums: list[int], k: int) -> int:
-        # current_sum = 0
-        # queue = deque()
-        # answer = 0
+        sums_count = {0: 1}
+        counter = 0
+        nums_sum = 0
+        for n in nums:
+            nums_sum += n
+            counter += sums_count.setdefault(nums_sum - k, 0)
+            sums_count[nums_sum] = sums_count.setdefault(nums_sum, 0) + 1
+        return counter
 
-        # for v in nums:
-        #     if v >= k:
-        #         if v == k:
-        #             answer += 1
-        #         current_sum = 0
-        #         queue.clear()
-        #     else:
-        #         current_sum += v
-        #         if current_sum == k:
-        #             answer += 1
-        #             current_sum -= queue.popleft()
-        #         queue.append(v)
-
-        # return answer
-        from collections import defaultdict  # Dictionary that returns a default value for non-existing keys.
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-        ans = 0
-        count = defaultdict(int)  # Maintain a count of each sum obtained so far. This "defaultdict" returns "0" by default.
-        count[0] = 1  # Initial sum of "0" encountered once at the beginning.
-        for i in range(len(nums)):
-            ans += count[nums[i] - k]  # Add the count of "nums[i] - k" encountered till now.
-            count[nums[i]] += 1  # Increment the count of the current sum "nums[i]".
-        return ans
 
 if __name__ == "__main__":
     inputs = (
-        # (([1, 1, 1], 2), 2),
-        # (([1, 2, 3], 3), 2),
-        # (([0], 0), 1),
+        (([1, 1, 1], 2), 2),
+        (([1, 2, 3], 3), 2),
+        (([0], 0), 1),
         (([1, 3, 1, 4, 4, 2, 1, 1, 2], 4), 6),
-        (([1, -1, 1, -1, 1, -1], 0), 4),
+        (([1, -1, 1, -1, 1, -1], 0), 9),
     )
 
     for input, expected in inputs:
