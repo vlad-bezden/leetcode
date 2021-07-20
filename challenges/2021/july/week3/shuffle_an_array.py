fa6eddd43acdc8d3a88738ca3ed870b754cc9690
Vlad Bezden <vlad.bezden@gmail.com>
2021-07-20 07:15:36 -0400

    Add solution for 'Shuffle an Array' daily challenge

---
+++ challenges/2021/july/week3/shuffle_an_array.py
@@ -0,0 +1,30 @@
+"""Shuffle an Array.
+
+    https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/610/week-3-july-15th-july-21st/3820/
+"""
+from random import sample
+
+
+class Solution:
+    def __init__(self, nums: list[int]) -> None:
+        self.nums = nums
+        self.len = len(nums)
+
+    def reset(self) -> list[int]:
+        """Resets the array to its original configuration and return it."""
+        return self.nums
+
+    def shuffle(self) -> list[int]:
+        """Returns a random shuffling of the array."""
+        return sample(self.nums, self.len)
+
+
+if __name__ in "__main__":
+    input = sample(range(100), 20)
+    print(input)
+    solution = Solution(input)
+    shuffle = solution.shuffle()
+    assert set(solution.shuffle()) == set(input), "Invalid shuffle."
+    assert solution.reset() == input, "Invalid reset"
+
+    print("PASSED!!!")
