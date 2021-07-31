c05f13ee32240c5eae36e161d8197efd36e5e9d8
Vlad Bezden <vlad.bezden@gmail.com>
2021-07-31 11:24:01 -0400

    Update typing

---
+++ libs/binary_tree.py
@@ -0,0 +1,33 @@
+from typing import Iterable
+
+
+class TreeNode:
+    def __init__(self, val: int, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return f"val: {self.val}, left: {self.left}, right: {self.right}"
+
+    def __str__(self) -> str:
+        return str(self.val)
+
+
+def to_binary_tree(items: Iterable[int]) -> TreeNode:
+    """Create BT from list of values."""
+    n = len(items)
+    if n == 0:
+        return None
+
+    def inner(index: int = 0) -> TreeNode:
+        """Closure function using recursion bo build tree."""
+        if n <= index or items[index] is None:
+            return None
+
+        node = TreeNode(items[index])
+        node.left = inner(2 * index + 1)
+        node.right = inner(2 * index + 2)
+        return node
+
+    return inner()
