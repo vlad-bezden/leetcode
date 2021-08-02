57de7d70bfd353636697f75629f8d022bfde0020
Vlad Bezden <vlad.bezden@gmail.com>
2021-08-02 07:38:29 -0400

    Update '1. Two Sum' problem

--- problems/1. Two Sum/two_sum.py
+++ problems/1. Two Sum/two_sum.py
@@ -1,35 +1,28 @@
-from typing import Optional
+"""1. Two Sum
+
+    https://leetcode.com/problems/two-sum/
+"""
 
 
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> Optional[list[int]]:
-        hash: dict[int, int] = {}
+    @staticmethod
+    def twoSum(nums: list[int], target: int) -> list[int]:
+        hash = {}
         for i, v in enumerate(nums):
             if (i2 := hash.get(target - v)) is not None:
                 return [i2, i]
             hash[v] = i
-        return None
+        return nums
 
 
 if __name__ == "__main__":
-    solution = Solution()
-
-    input = [2, 7, 11, 15]
-    target = 9
-    expected = [0, 1]
-    output = solution.twoSum(input, target)
-    assert output == expected, f"{expected=}, {output=}"
-
-    input = [3, 2, 4]
-    target = 6
-    expected = [1, 2]
-    output = solution.twoSum(input, target)
-    assert output == expected, f"{expected=}, {output=}"
-
-    input = [3, 3]
-    target = 6
-    expected = [0, 1]
-    output = solution.twoSum(input, target)
-    assert output == expected, f"{expected=}, {output=}"
+    inputs = (
+        ({"nums": [2, 7, 11, 15], "target": 9}, [0, 1]),
+        ({"nums": [3, 2, 4], "target": 6}, [1, 2]),
+        ({"nums": [3, 3], "target": 6}, [0, 1]),
+    )
+    for input, expected in inputs:
+        output = Solution.twoSum(**input)
+        assert output == expected, f"{input=}, {output=}, {expected=}"
 
     print("PASSED!!!")
