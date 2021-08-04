3eac169efa2449f86c406554a0e79d3234ce4221
Vlad Bezden <vlad.bezden@gmail.com>
2021-08-03 20:34:48 -0400

    Add solution for '90. Subsets II' problem

---
+++ problems/90. Subsets II/subsets_II.py
@@ -0,0 +1,55 @@
+"""90. Subsets II.
+
+    https://leetcode.com/problems/subsets-ii/
+"""
+
+
+class Solution:
+    @staticmethod
+    def subsetsWithDup(nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        ans = []
+        length = len(nums)
+
+        def helper(index, sublist):
+            ans.append(sublist)
+            # base case
+            if index >= length:
+                return
+            s = set()
+            for i, v in enumerate(nums[index:], start=index):
+                if v not in s:
+                    s.add(v)
+                    helper(i + 1, sublist + [v])
+
+        helper(0, [])
+        return ans
+
+
+if __name__ == "__main__":
+    inputs = (
+        ([1, 2, 2], [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]),
+        ([0], [[], [0]]),
+        ([1, 2, 3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]),
+        (
+            [4, 4, 4, 1, 4],
+            [
+                [],
+                [1],
+                [1, 4],
+                [1, 4, 4],
+                [1, 4, 4, 4],
+                [1, 4, 4, 4, 4],
+                [4],
+                [4, 4],
+                [4, 4, 4],
+                [4, 4, 4, 4],
+            ],
+        ),
+    )
+
+    for input, expected in inputs:
+        output = Solution.subsetsWithDup(input)
+        assert output == expected, f"{input=}, {output=}, {expected=}"
+
+    print("PASSED!!!")
