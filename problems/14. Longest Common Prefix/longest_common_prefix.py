f7935c4f5035210be73401fe2f03919045fa04cb
Vlad Bezden <vlad.bezden@gmail.com>
2021-08-08 15:55:29 -0400

    Add solution for '14. Longest Common Prefix' problem

---
+++ problems/14. Longest Common Prefix/longest_common_prefix.py
@@ -0,0 +1,34 @@
+"""14. Longest Common Prefix.
+
+https://leetcode.com/problems/longest-common-prefix/
+"""
+
+
+class Solution:
+    @staticmethod
+    def longestCommonPrefix(strings: list[str]) -> str:
+        if len(strings) == 1:
+            return strings[0]
+        answer = []
+        for chars in zip(*strings):
+            if len(set(chars)) != 1:
+                break
+            answer.append(chars[0])
+        return "".join(answer)
+
+
+if __name__ == "__main__":
+    inputs = (
+        (["", ""], ""),
+        (["flower", ""], ""),
+        (["flower", "flow", "flight"], "fl"),
+        (["dog", "racecar", "car"], ""),
+        (["dog", "dogma", "dogtrine"], "dog"),
+        (["dog", "doom", "drum"], "d"),
+    )
+
+    for input, expected in inputs:
+        output = Solution.longestCommonPrefix(input)
+        assert output == expected, f"{input=}, {output=}, {expected=}"
+
+    print("PASSED!!!")
