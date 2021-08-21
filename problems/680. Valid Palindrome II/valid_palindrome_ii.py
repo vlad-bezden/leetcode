19f5ccb2e1726494eff0a03bf9d7cab057661ce8
vlad-bezden <vlad.bezden@gmail.com>
2021-08-21 14:31:51 -0400

    Refactor '680. Valid Palindrome II' problem

--- problems/680. Valid Palindrome II/valid_palindrome_ii.py
+++ problems/680. Valid Palindrome II/valid_palindrome_ii.py
@@ -3,11 +3,9 @@ class Solution:
     def validPalindrome(text: str) -> bool:
         del_char = lambda i: text[:i] + text[i + 1:]
         is_palindrome = lambda s: s == s[::-1]
-        if is_palindrome(text):
-            return True
         l = len(text)
         mid = l // 2
-        for i, j in zip(range(mid), range(l - 1, mid, -1)):
+        for i, j in zip(range(mid), reversed(range(mid, l))):
             # first char that is not the same on both sides
             if text[i] != text[j]:
                 return is_palindrome(del_char(i)) or is_palindrome(del_char(j))
