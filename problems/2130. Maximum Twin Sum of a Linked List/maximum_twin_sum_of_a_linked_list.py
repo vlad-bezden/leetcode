f3922ede16e03a24791f6804ee44123bb5fa3c34
Vlad Bezden <vbezden@marqeta.com>
2022-04-20 12:28:21 -0400

    Update '2130. Maximum Twin Sum of a Linked List' with the second solution

--- problems/2130. Maximum Twin Sum of a Linked List/maximum_twin_sum_of_a_linked_list.py
+++ problems/2130. Maximum Twin Sum of a Linked List/maximum_twin_sum_of_a_linked_list.py
@@ -16,7 +16,12 @@ class ListNode:
 
 class Solution:
     @staticmethod
-    def pairSum(head: Optional[ListNode]) -> int:
+    def pairSum_1(head: Optional[ListNode]) -> int:
+        """Using list to load data.
+
+        Speed complexity 0(n)
+        Memory complexity 0(2n)
+        """
         ll_vals = []
         result = 0
 
@@ -26,5 +31,31 @@ class Solution:
 
         for i in range(len(ll_vals) // 2):
             result = max(result, (ll_vals[i] + ll_vals[-1 - i]))
+
+        return result
+
+    @staticmethod
+    def pairSum_2(head: Optional[ListNode]) -> int:
+        """Using two pointers.
+
+        Run to the middle of the LL and the same time
+        reverse first part LL. Then from the middle of LL
+        use both pointers to take values one forward and second
+        backward.
+        """
+        prev, slow, fast = None, head, head
+        result = 0
+
+        while fast and fast.next:
+            fast = fast.next.next
+            next_ = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_
+
+        while slow:
+            result = max(result, slow.val + prev.val)
+            slow = slow.next
+            prev = prev.next
 
         return result
