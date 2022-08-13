eaf3b9fe2580227d361b3cb42d1ab456ec691eae
Vlad Bezden <vbezden@marqeta.com>
2022-08-13 17:50:13 -0400

    Add solution for '12. Integer to Roman' problem

---
+++ problems/12. Integer to Roman/integer_to_roman.py
@@ -0,0 +1,53 @@
+"""12. Integer to Roman.
+
+Level: Medium
+
+https://leetcode.com/problems/integer-to-roman/
+"""
+
+
+class Solution:
+    INT_TO_ROMAN = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        3: "III",
+        2: "II",
+        1: "I",
+    }
+
+    @staticmethod
+    def intToRoman(num: int) -> str:
+        result = []
+        for k, v in Solution.INT_TO_ROMAN.items():
+            q, r = divmod(num, k)
+            if q:
+                result.append(q * v)
+                num = r
+        return "".join(result)
+
+
+if __name__ == "__main__":
+    tests = (
+        (7, "VII"),
+        (3, "III"),
+        (58, "LVIII"),
+        (78, "LXXVIII"),
+        (1994, "MCMXCIV"),
+        (1973, "MCMLXXIII"),
+    )
+    for test in tests:
+        input, expected = test
+        output = Solution.intToRoman(input)
+        assert output == expected, f"{input =}, {output =}, {expected =}"
+
+    print("PASSED!!!")
