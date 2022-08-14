e816138796d2538a42e3ca1c8693d31c4aaa068b
Vlad Bezden <vbezden@marqeta.com>
2022-08-14 07:32:39 -0400

    Refactore '12. Integer to Roman' problem

--- problems/12. Integer to Roman/integer_to_roman.py
+++ problems/12. Integer to Roman/integer_to_roman.py
@@ -7,32 +7,34 @@ https://leetcode.com/problems/integer-to-roman/
 
 
 class Solution:
-    INT_TO_ROMAN = {
-        1000: "M",
-        900: "CM",
-        500: "D",
-        400: "CD",
-        100: "C",
-        90: "XC",
-        50: "L",
-        40: "XL",
-        10: "X",
-        9: "IX",
-        5: "V",
-        4: "IV",
-        3: "III",
-        2: "II",
-        1: "I",
-    }
+    INT_TO_ROMAN = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (3, "III"),
+        (2, "II"),
+        (1, "I"),
+    ]
 
     @staticmethod
     def intToRoman(num: int) -> str:
         result = []
-        for k, v in Solution.INT_TO_ROMAN.items():
-            q, r = divmod(num, k)
+        for k, v in Solution.INT_TO_ROMAN:
+            # exit if there is no more conversion left
+            if num == 0:
+                break
+            q, num = divmod(num, k)
             if q:
                 result.append(q * v)
-                num = r
         return "".join(result)
 
 
